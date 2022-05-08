fdfe516d6d24a6f74347a1e12c996b9ac90b860b
doanh11 <trancongdoanh150896@gmail.com>
2022-05-08 23:20:22 +0700

    upbai

--- QuanLySinhVien.js
+++ QuanLySinhVien.js
@@ -1,138 +1,56 @@
 function coutn() {
-    // B1 : DOM và lấy giá trị của input
-    var diemKhuVuc = +document.getElementById ("diemkhuvuc").value ;
-    // var isKhuVucB = document.getElementById ("khuvucB").checked ;
-    // var isKhuVucC = document.getElementById ("khuvucC").checked ;
-    // var isKhuVucX = document.getElementById ("khuvucX").checked ;
-    
-    var diemDoiTuong = +document.getElementById ("doituong").value ;
-    // var isdoiTuong2 = document.getElementById ("doiTuong2").checked ;
-    // var isdoiTuong3 = document.getElementById ("doiTuong3").checked ;
-    // var isdoiTuongX = document.getElementById ("doiTuongX").checked ;
-    
+    var diemKhuVuc = document.getElementById ("khuvuc").value ;
+    var diemDoiTuong = document.getElementById ("doituong").value ;
     var mon1 = +document.getElementById ("mon1").value ;
     var mon2 = +document.getElementById ("mon2").value ;
     var mon3 = +document.getElementById ("mon3").value ;
     var diemChuan = +document.getElementById ("diemChuan").value ;
-    var tongDiem = mon1 + mon2 + mon3 + diemKhuVuc + diemDoiTuong ; 
-    if (mon1 === 0 || mon2 === 0 || mon3 === 0){
-        document.getElementById ("ketQua").innerHTML = `bạn đã thi trượt với một môn điểm bằng 0  với tổng điểm ${tongDiem}`
-    }else if (diemChuan > tongDiem) {
-        document.getElementById ("ketQua").innerHTML = `bạn đã thi rớt với tổng điểm ${tongDiem}`
-    }else {
-        document.getElementById ("ketQua").innerHTML = `bạn đã thi đậu với tổng điểm ${tongDiem}`
-
-    }
-    // var diemKhuVuc =  checkArea() ;
-    // var diemDoiTuong = checkSubject () ;
-    // var diemBaMon = aulculatePoint () ;
-    // var total= 0 ;
-    // B2 kiểm tra user đang chọn khu vực nào và chọn đối tượng nào để áp dụng công thức
-    // khu vực A
-//     if (isKhuVucA) {
-//         // cộng điểm khu vực A
-//         if (isdoiTuong1) {
-//             total += mon1 + mon2 + mon3 + 2 + 2.5 ;
-//         }else if (isdoiTuong2) {
-//             total += mon1 + mon2 + mon3 + 2 + 1.5 ;
-//         }else if (isdoiTuong3) {
-//             total += mon1 + mon2 + mon3 + 2 + 1 ;
-//         } else {
-//             total +=  mon1 + mon2 + mon3 + 2 + 0 ;
-//         }
-//     }else if (isKhuVucB) {
-//         // khu vực B
-//         // cộng điểm khu vực B
-//         if (isdoiTuong1) {
-//             total += mon1 + mon2 + mon3 + 1 + 2.5 ;
-//         }else if (isdoiTuong2) {
-//             total += mon1 + mon2 + mon3 + 1 + 1.5 ;
-//         }else if (isdoiTuong3) {
-//             total += mon1 + mon2 + mon3 + 1 + 1 ;
-//         }else {
-//             total +=  mon1 + mon2 + mon3 + 1 + 0 ;
-//         }
-//     }else if (isKhuVucC) {
-//         // khu vực C 
-//         // cộng điểm khu vực C
-//         if (isdoiTuong1) {
-//             total += mon1 + mon2 + mon3 + 0.5 + 2 ;
-//         }else if (isdoiTuong2) {
-//             total += mon1 + mon2 + mon3 + 0.5 + 1.5 ;
-//         }else if (isdoiTuong3) {
-//             total += mon1 + mon2 + mon3 + 0.5 + 1 ;
-//         }else {
-//             total +=  mon1 + mon2 + mon3 + 0.5 + 0 ;
-//         }
-//     }else {
-//         // khu vực không ưu tiên
-//         // không cộng điểm;
-//         total += mon1 + mon2 + mon3 ;
-//     }
-// //  tổng điểm 
-//     // total = diemKhuVuc + diemDoiTuong + diemBaMon ; 
-//     // alert (total)
-
-//     // alert (diemChuan);
-
-//     // B3 :  kiểm tra tổng điểm của user và so sánh với điểm chuẩn và xuất kết quả
-//     if(diemChuan < point && mon1 > 0 && mon2 > 0 && mon3 > 0  ) {
-//         document.getElementById ("ketQua").innerHTML = `tổng điểm ${total} Đậu`
-//     }else {
-//         document.getElementById ("ketQua").innerHTML = `tổng điểm ${total} Rớt`
-//     }
-// var khuvuc = checkArea(khuvuc)
+    diemKhuVuc = checkArea(diemKhuVuc) ;
+    diemDoiTuong = checkSubject(diemDoiTuong) ;
+    var tongDiem3Mon =  aulculatePoint(mon1,mon2,mon3) ;
+    var diemTongKet= diemKhuVuc + diemDoiTuong + tongDiem3Mon ;
+    hienThiKetQua (mon1,mon2,mon3,diemTongKet,diemChuan) ;
 }
-
-// // Hàm kiểm tra  khu vực 
-// function checkArea(khuvuc){
-//     var isKhuVucA = document.getElementById ("khuvucA").checked ;
-//     var isKhuVucB = document.getElementById ("khuvucB").checked ;
-//     var isKhuVucC = document.getElementById ("khuvucC").checked ;
-//     var isKhuVucX = document.getElementById ("khuvucX").checked ;
-    
-//     if(isKhuVucA) {
-//     return 2;
-//     }
-//      if (isKhuVucB){
-//     return 1;
-//     }
-//     if (isKhuVucC) {
-//      return 0.5; 
-//     }
-//     if (isKhuVucX){
-//         return 0 ;
-//     }
-//     // alert (point);
-// }
-// var diemKhuVuc =  checkArea(khuvuc) ;
-// console.log (diemKhuVuc) ;
-
-
+// Hàm kiểm tra  khu vực 
+function checkArea(diemKhuVuc){
+   switch (diemKhuVuc) {
+        case "A" :
+         return 2;
+        case "B" :
+         return 1 ;
+        case "C" :
+         return 0.5
+        default :
+        return 0;   
+   }
+}
 // hàm kiểm tra đối tượng
-// function checkSubject(){
-//     var isdoiTuong1 = document.getElementById ("doiTuong1").checked ;
-//     var isdoiTuong2 = document.getElementById ("doiTuong2").checked ;
-//     var isdoiTuong3 = document.getElementById ("doiTuong3").checked ;
-//     var isdoiTuongX = document.getElementById ("doiTuongX").checked ;
-//     var point = 0 ;
-//     if (isdoiTuong1){
-//         point += 2.5 ;
-//     }else if (isdoiTuong2) {
-//         point += 1.5 ;
-//     }else if (isdoiTuong3) {
-//         point += 1 ;
-//     }else {
-//         point +=0 ;
-//     }
-//     alert (point)
-// }
+function checkSubject(diemDoiTuong){
+   switch (diemDoiTuong) {
+        case "doituong1" :
+        return 2.5 ;
+        case "doituong2" :
+        return 1.5 ;
+        case "doituong3" :
+        return 1 ;
+        default :
+        return 0 ;
+   }
+}
 // hàm tính tổng điểm 3 môn
-// function aulculatePoint() {
-//     var mon1 = +document.getElementById ("mon1").value ;
-//     var mon2 = +document.getElementById ("mon2").value ;
-//     var mon3 = +document.getElementById ("mon3").value ;
-//     var point = mon1 + mon2 + mon3;
-    
-// }
-
+function aulculatePoint(mon1,mon2,mon3) {
+    return mon1 + mon2 + mon3 ;
+}
+// hàm hiển thị kết quả 
+function hienThiKetQua (mon1,mon2,mon3,diemTongKet,diemChuan){
+    if(!mon1 || !mon2 || !mon3){
+        document.getElementById ("ketQua").style.color = "red"
+        document.getElementById ("ketQua").innerHTML = "Bạn đã thi trượt vì có 1 môn 0 điểm"
+    }else if (diemTongKet >= diemChuan ) {
+        document.getElementById ("ketQua").style.color = "blue"
+        document.getElementById ("ketQua").innerHTML = `Bạn đã thi đậu với tổng điểm ${diemTongKet}`
+    }else {
+        document.getElementById ("ketQua").style.color = "red"
+        document.getElementById ("ketQua").innerHTML = `Bạn đã thi rớt với tổng điểm ${diemTongKet}`
+    }
+}
--- TinhTienDien.js
+++ TinhTienDien.js
@@ -2,37 +2,38 @@ function countElectricity(){
     // B1 : DOM lấy giá trị của input
     var kw = +document.getElementById ("numberKw").value ;
     var name = document.getElementById ("name").value ;
-    
-    //B2 : tính toán giá tiền điện theo từng giá trị
     var total = 0 ;
-    if (kw <= 50) {
-        total += kw * 500 ;
-    }else if ( kw<= 100 ) {
-        total += 50 * 500 + (kw - 50) * 650 ;
-    }else if (kw <= 200 ) {
-        total += 50 * 500 + (50 * 650) + ((kw -100) * 850) ;
-    }else if (kw <= 350 ) {
-        total += 50 * 500 + (50 * 650) + ( 100 * 850) + ((kw - 200) * 1100);
-    }else {
-        total +=50 * 500 + (50 * 650) + (100* 850) + (150 * 1100) + (kw - 350) * 1300 ;
+    //B2 : tính toán giá tiền điện theo từng giá trị
+    total = calculate(kw, 500, 650, 850, 1100, 1300);
+    // if (kw <= 50) {
+    //     total += kw * 500 ;
+    // }else if ( kw<= 100 ) {
+    //     total += 50 * 500 + (kw - 50) * 650 ;
+    // }else if (kw <= 200 ) {
+    //     total += 50 * 500 + (50 * 650) + ((kw -100) * 850) ;
+    // }else if (kw <= 350 ) {
+    //     total += 50 * 500 + (50 * 650) + ( 100 * 850) + ((kw - 200) * 1100);
+    // }else {
+    //     total +=50 * 500 + (50 * 650) + (100* 850) + (150 * 1100) + (kw - 350) * 1300 ;
         
-    }
+    // }
     // B3 : xuất kết quả
-    document.getElementById ("tongTien").innerHTML = `Người dùng : ${name} : ${total}
-     vnd`
+    document.getElementById ("tongTien").innerHTML = `<p>USER: ${name} </p> <p> Tổng số tiền phải trả ${total}
+     vnd </p>`
 
-// function calculate(kw,kw_50,kw_50_100,kw_100_200,kw_200_350,kw_350){
-//     var total = 0 ;
-//     if (kw <= 50) {
-//         total += kw * kw_50 ;
-//     }else if (kw <= 100) {
-//         total += 50 * kw_50 + (kw - 50) * kw_50_100 ;
-//     }else if (kw <= 200){
-//         total += 50 * kw_50 + (100 * kw_50_100) + ((kw-100) * kw_100_200) ;
-//     }else if (kw <= 350 ){
-//         total += 50 * kw_50 + (100 *kw_50_100) + (50 * kw_100_200) + ((kw - 200)* kw_200_350) ;
-//     }else {
-//         total += 50 * kw_50 + (100 *kw_50_100) + (50 * kw_100_200) + (150 * kw_200_350 ) + ((kw - 350) * kw_350)
-//     }
-
-}
+    }
+    function calculate(kw,kw_50,kw_650,kw_850,kw_1100,kw_1300){
+        var total = 0 ;
+        if (kw <= 50) {
+            total += kw * kw_50 ;
+        }else if (kw <= 100) {
+            total += 50 * kw_50 + (kw - 50) * kw_650;
+        }else if (kw <= 200){
+            total += 50 * kw_50 + (50 * kw_650) + ((kw-100) * kw_850) ;
+        }else if (kw <= 350 ){
+            total += 50 * kw_50 + (50 *kw_650) + (100 * kw_850) + ((kw - 200)* kw_1100) ;
+        }else {
+            total += 50 * kw_50 + (50 *kw_650) + (100 * kw_850) + (150 * kw_1100 ) + ((kw - 350) * kw_1300)
+        }
+        return total ;
+    }
