8baeefb20bcadefd168e9a9d6778050ac7613f67
doanh11 <trancongdoanh150896@gmail.com>
2022-05-11 10:17:15 +0700

    upbai

--- TinhTienCap.js
+++ TinhTienCap.js
@@ -3,10 +3,43 @@ function tienCap (){
     var maKhach = +document.getElementById ("makhachhang").value ;
     var ketNoi = + document.getElementById ("soketnoi").value ;
     var soKenh = +document.getElementById ("sokenhCaoCap").value ;
-
-    if (khachHang === "doanhNghiep") {
-        document.getElementById("soketnoi").style.display = "block" ;
+    var tienCapNhaDan = capNhaDan(soKenh) ;
+    var tienCapDoanhNghiep =  capDoanhNghiep(ketNoi,soKenh) ;
+    var soTienCap = document.getElementById ("tienCap") ;
+//     hiển thị kết quả 
+    if (khachHang === "loaikhachhang"){
+        alert("vui lòng chọn loại khách hàng")
+    }else if (khachHang === "nhaDan"){
+        soTienCap.innerHTML = `Mã khách hàng : ${maKhach} ; Tiền cáp $ ${tienCapNhaDan}`
     }else {
+        soTienCap.innerHTML = `Mã khách hàng : ${maKhach} ; Tiền cáp $ ${tienCapDoanhNghiep}`  
+    }
+
+}
+
+// hàm chọn loại khách hàng
+function khDoanhNghiep() {
+    var khachHang = document.getElementById ("loaikhachhang").value ;
+    if (khachHang === "loaikhachhang"){
+        alert("vui lòng chọn loại khách hàng")
+    }else if (khachHang === "doanhNghiep") {
+        document.getElementById("soketnoi").style.display = "block" ;
+    }else{
         document.getElementById("soketnoi").style.display = "none" ;
     }
+}
+// hàm tính tiền cáp nhà dân 
+function capNhaDan(soKenh) {
+    return 4.5 + 20.5 + soKenh * 7.5;  
+
+}
+// hàm tính tiền cáp doanh nghiệp
+function capDoanhNghiep(ketNoi,soKenh){
+    var total = 0 ;
+    if (ketNoi <= 10){
+        total = 15 + 75 + soKenh * 50 ; 
+    }else {
+        total = 15 + 75 + soKenh * 50 + (ketNoi - 10) * 5;
+    }
+    return total ; 
 }
